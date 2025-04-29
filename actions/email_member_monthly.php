02063342240ddcab9eea63f38896f7dffb0cf5cc
Andrew Breakspear <andrew@longhanborough.com>
2025-04-29 10:43:13 +0100

    Email summary notifcations

--- actions/email_member_monthly.php
+++ actions/email_member_monthly.php
@@ -2,11 +2,12 @@
 include_once("../inc/autoload.php");
 
 // get start/end of last month
-$start = date('Y-m-d', strtotime("first day of this month"));
-$end = date('Y-m-d', strtotime("last day of this month"));
+$start = date('Y-m-d', strtotime('monday last week'));
+$end = date('Y-m-d', strtotime('sunday last week'));
 
 $sql = "SELECT * FROM staff WHERE email IS NOT NULL AND email != '' ORDER BY lastname ASC, firstname ASC";
 $staffAll = $db->get($sql);
+$sendFrom = setting('email_sender_address');
 
 foreach ($staffAll as $staff) {
 	$staff = new Staff($staff['uid']);
@@ -21,35 +22,39 @@ foreach ($staffAll as $staff) {
 	
 	if ($totalMinutesRoundedUp > 0) {
 		
-		$output  = "<h1>" . $staff->fullname() . "</h1>";
-		$output .= "<p>Dear " . $staff->firstname . ",</p>";
-		$output .= "<p>Please find below a summary of the shifts you have worked at St Edmund Hall during the period from " . $start . " to " . $end . ":</p>";
+		//$message  = "<h1>" . $staff->fullname() . "</h1>";
+		$message  = "<p>Dear " . $staff->firstname . ",</p>";
+		$message .= "<p>Please find below a summary of the shifts you have worked at St Edmund Hall between the " . date('dS \of F', strtotime($start)) . " to the " . date('dS \of F', strtotime($end)) . ":</p>";
 		
-		$output .= "<p>Total Shifts: " . count($shifts) . "<br/>";
-		$output .= "Total Hours Worked: " . convertMinutesToHours($totalMinutesRoundedUp) . "</p>";
-		$output .= "<p>Summary:</p>";
-		$output .= "<ul>";
+		$message .= "<p>Total Shifts: " . count($shifts) . "<br/>";
+		$message .= "Total Hours Worked: " . convertMinutesToHours($totalMinutesRoundedUp) . "</p>";
+		$message .= "<p>Summary:</p>";
+		$message .= "<ul>";
+		
 		foreach ($shifts AS $shift) {
 			$shift = new Shift($shift['uid']);
 			
-			$output .= "<li>" . dateDisplay($shift->shift_start) . " - " .  convertMinutesToHours($shift->totalMinutesRoundedUp()) . "</li>";
+			$message .= "<li>" . dateDisplay($shift->shift_start) . " - " .  convertMinutesToHours($shift->totalMinutesRoundedUp()) . "</li>";
 		}
 		
-		$output .= "</ul>";
+		$message .= "</ul>";
 		
-		$output .= "<p>If you have any questions about the information above or notice anything that seems incorrect, please don’t hesitate to get in touch.</p>";
+		$message .= "<p>If you have any questions about the information above or notice anything that seems incorrect, please don’t hesitate to get in touch.</p>";
 		
-		$output .= "<p>Thank you for your continued support and hard work — it's very much appreciated.</p>";
+		$message .= "<p>Best regards,</p>";
 		
-		$output .= "<p>Best regards,</p>";
+		$to = $staff->email;
+		$subject = 'Your shifts summary';
+		// Set content-type header for HTML email
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
 		
+		// Additional headers
+		$headers .= 'From: ' . $sendFrom . "\r\n";
+		$headers .= 'Reply-To: ' . $sendFrom . "\r\n";
+		$headers .= 'X-Mailer: PHP/' . phpversion();
 		
-		
-		//$output .= "Emailing to: " . $staff->email;
-		echo $output;
+		mail($to, $subject, $message, $headers);
 	}
-	
 }
-
-
 ?>
