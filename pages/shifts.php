e4f395ab9460f31a5e7530a34ce1bfea45591388
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2026-04-18 11:13:28 +0100

    Default date range for shifts

--- pages/shifts.php
+++ pages/shifts.php
@@ -52,57 +52,85 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	}
 }
 
-$sql = "SELECT * FROM shifts ORDER BY uid DESC";
-$shiftsAll = $db->get($sql);
+function parseShiftFilterDate($value) {
+	if (!is_string($value) || !preg_match('/^\d{4}-\d{2}-\d{2}$/', $value)) {
+		return null;
+	}
+
+	$date = DateTimeImmutable::createFromFormat('Y-m-d', $value);
+	$errors = DateTimeImmutable::getLastErrors();
+
+	if ($date === false) {
+		return null;
+	}
+
+	if (is_array($errors) && ($errors['warning_count'] > 0 || $errors['error_count'] > 0)) {
+		return null;
+	}
+
+	return $date;
+}
+
+$today = new DateTimeImmutable('today');
+$defaultTo = $today;
+$defaultFrom = $today->modify('-29 days');
+
+$fromInput = $_GET['from'] ?? $defaultFrom->format('Y-m-d');
+$toInput = $_GET['to'] ?? $defaultTo->format('Y-m-d');
+
+$fromDate = parseShiftFilterDate($fromInput);
+$toDate = parseShiftFilterDate($toInput);
+
+if (!$fromDate || !$toDate) {
+	echo alert('warning', 'Invalid date range', 'Showing the last 30 days instead.');
+	$fromDate = $defaultFrom;
+	$toDate = $defaultTo;
+}
+
+if ($fromDate > $toDate) {
+	[$fromDate, $toDate] = [$toDate, $fromDate];
+}
+
+$fromValue = $fromDate->format('Y-m-d');
+$toValue = $toDate->format('Y-m-d');
+
+$sql = "SELECT * FROM shifts WHERE DATE(shift_start) BETWEEN :from AND :to ORDER BY shift_start DESC, uid DESC";
+$shiftsAll = $db->query($sql, [
+	'from' => $fromValue,
+	'to' => $toValue,
+]);
 ?>
 
 <h1><?php echo icon('hourglass-split', '1em'); ?> Shifts</h1>
-<div class="pb-3 text-end">
-	<a class="btn btn-success" href="index.php?page=shift_edit" role="button"><?php echo icon('hourglass-split'); ?> Add New</a>
+<div class="row g-3 align-items-end pb-3">
+	<div class="col-12 col-lg">
+		<form action="index.php" method="get" class="row g-3 align-items-end">
+			<input type="hidden" name="page" value="shifts">
+			<div class="col-12 col-md-4">
+				<label for="from" class="form-label">From</label>
+				<input type="date" class="form-control" id="from" name="from" value="<?php echo htmlspecialchars($fromValue); ?>" max="<?php echo htmlspecialchars($toValue); ?>">
+			</div>
+			<div class="col-12 col-md-4">
+				<label for="to" class="form-label">To</label>
+				<input type="date" class="form-control" id="to" name="to" value="<?php echo htmlspecialchars($toValue); ?>" min="<?php echo htmlspecialchars($fromValue); ?>">
+			</div>
+			<div class="col-12 col-md-auto">
+				<button class="btn btn-outline-secondary w-100" type="submit">Apply</button>
+			</div>
+		</form>
+	</div>
+	<div class="col-12 col-lg-auto text-end">
+		<a class="btn btn-success" href="index.php?page=shift_edit" role="button"><?php echo icon('hourglass-split'); ?> Add New</a>
+	</div>
 </div>
 
 <?php
-// Set reference dates in 'Y-m-d' format
-$todayStr = (new DateTime('today'))->format('Y-m-d');
-$yesterdayStr = (new DateTime('yesterday'))->format('Y-m-d');
-$mondayThisWeekStr = (new DateTime('monday this week'))->format('Y-m-d');
-$mondayLastWeekStr = (new DateTime('monday this week'))->modify('-7 days')->format('Y-m-d');
+echo '<p class="text-muted">Showing ' . count($shiftsAll) . ' shifts from ' . htmlspecialchars($fromDate->format('j M Y')) . ' to ' . htmlspecialchars($toDate->format('j M Y')) . '.</p>';
 
-// Initialise groups
-$grouped = [
-	'Today' => [],
-	'Yesterday' => [],
-	'This Week' => [],
-	'Last Week' => [],
-	'Older' => [],
-];
-
-foreach ($shiftsAll as $shiftData) {
-	$shift = new Shift($shiftData['uid']);
-	
-	// Extract just the date part of shift_start
-	$shiftDateStr = (new DateTime($shift->shift_start))->format('Y-m-d');
-
-	if ($shiftDateStr === $todayStr) {
-		$grouped['Today'][] = $shift;
-	} elseif ($shiftDateStr === $yesterdayStr) {
-		$grouped['Yesterday'][] = $shift;
-	} elseif ($shiftDateStr >= $mondayThisWeekStr) {
-		$grouped['This Week'][] = $shift;
-	} elseif ($shiftDateStr >= $mondayLastWeekStr && $shiftDateStr < $mondayThisWeekStr) {
-		$grouped['Last Week'][] = $shift;
-	} else {
-		$grouped['Older'][] = $shift;
-	}
-}
-
-// Output the grouped shifts
-foreach ($grouped as $label => $shiftsGroup) {
-	if (empty($shiftsGroup)) continue;
-
-	$table  = "<h2>{$label}</h2>";
-	
-	$table .= "<table class=\"table mb-5\">";
+if (empty($shiftsAll)) {
+	echo alert('info', 'No shifts found', 'There are no shifts in the selected date range.');
+} else {
+	$table  = "<table class=\"table mb-5\">";
 	$table .= "<thead>";
 	$table .= "<tr>";
 	$table .= "<th scope=\"col\">Name</th>";
@@ -113,14 +141,15 @@ foreach ($grouped as $label => $shiftsGroup) {
 	$table .= "</tr>";
 	$table .= "</thead>";
 	$table .= "<tbody>";
-	
-	foreach ($shiftsGroup as $shift) {
+
+	foreach ($shiftsAll as $shiftData) {
+		$shift = new Shift($shiftData['uid']);
 		$table .= $shift->tableRow();
 	}
-	
+
 	$table .= "</tbody>";
 	$table .= "</table>";
-	
+
 	echo $table;
 }
 ?>
